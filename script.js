1f3fa7e6fc1b421c2f901ae419f2da7c44b39e5a
ILYES <abderrahmaneelyes66@gmail.com>
2025-05-03 12:40:57 +0100

    con

--- script.js
+++ script.js
@@ -1,24 +1,78 @@
-let menu =document.getElementsByClassName('hidden')[0]
-const menubtn =document.getElementsByClassName('menu')[0]  
 
-console.log(menu)
-console.log(menubtn)
+let ActiveImageIndex = 0;
+
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+const slider = document.getElementById('slider-img');
+const descText = document.getElementById('desc');
+const indexText = document.getElementById('index');
 
 
-let menuIsHidden=true
 
-menubtn.addEventListener('click', function(){
+slider.setAttribute('src', data[ActiveImageIndex].image);
+descText.innerText = data[ActiveImageIndex].title;
+indexText.textContent = " " + ActiveImageIndex;
+
+
+leftBtn.addEventListener('click', function () {
     
-    console.log("click",menuIsHidden)
-    console.log(menu.className)
+    if (ActiveImageIndex === data.length - 1) {  
+        rightBtn.setAttribute('disabled', true);
+        console.log('atteint le maximum ');
+    } else {
+        leftBtn.removeAttribute("disabled");
+        ActiveImageIndex++;
+    }
+    slider.setAttribute('src', data[ActiveImageIndex].image);
+    descText.innerText = data[ActiveImageIndex].title;
+    indexText.textContent = " " + ActiveImageIndex;
+    // console.log('Left button clicked. ', object[ActiveImageIndex]);
+});
 
-if(menuIsHidden){
-    menu.className = menu.className.replace('hidden', '  ');
-}else{
-    menu.className = menu.className + ' hidden'
-}
+
+rightBtn.addEventListener('click', function () {
+    
+
+   
+    if (ActiveImageIndex === data.length - 1) {
+        rightBtn.setAttribute('disabled',true)
+        console.log('atteint le maximum ')
+      
+     } else {
+         leftBtn.removeAttribute("disabled")
+         ActiveImageIndex++;}
+
+         slider.setAttribute('src', data[ActiveImageIndex].image);
+         descText.innerText = data[ActiveImageIndex].title;
+    indexText.textContent = " " + ActiveImageIndex;
+
+    // console.log('Right button clicked. ', object[ActiveImageIndex]);
+});
 
 
-menuIsHidden= !menuIsHidden;
 
-})
+
+todoBtn.addEventListener('click', function() {
+    console.log("Button Clicked");
+    console.log(todoInput.value);
+
+    // Créer le <li>
+    const NewElement = document.createElement('li');
+    NewElement.innerText = todoInput.value;
+
+    // Créer le bouton delete
+    const deleteBtn = document.createElement('button');
+    deleteBtn.innerText = "Supprimer";
+    deleteBtn.style.marginLeft = '10px';
+
+    // Ajouter un event listener pour supprimer le <li>
+    deleteBtn.addEventListener('click', function() {
+        NewElement.remove();
+    });
+
+    // Ajouter le bouton au <li>
+    NewElement.appendChild(deleteBtn);
+
+    // Ajouter le <li> à la liste
+    todoUl.appendChild(NewElement);
+});
